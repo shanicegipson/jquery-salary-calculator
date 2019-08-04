b26c73d35c11962b6b73683db5dcc3fd27239a72
Shanice Gipson <shanicegipson13@gmail.com>
2019-08-03 22:50:26 -0500

    added code

--- client.js
+++ client.js
@@ -1,1 +1,46 @@
-console.log('JS');
+const employees = [];
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('in readyNow');
+
+    render();
+
+    $('.js-btn-employee').on('click', addEmployee);
+}
+
+
+function addEmployee() {
+    const newEmployeeSalary = {
+        firstName: $('.js-input-first').val(),
+        lastName: $('.js-input-last').val(),
+        idNumber: $('.js-input-id').val(),
+        titlePosition: ($('.js-input-title').val()),
+        salary: $('.js-input-salary').val(),
+    }
+    console.log(newEmployeeSalary);
+
+    employees.push(newEmployeeSalary);
+
+    render();
+
+}
+
+function render() {
+    console.log('render');
+    const tableElement = $('.js-table');
+
+    tableElement.empty();
+    for (let i = 0; i < employees.length; i++) {
+        const employee = employees[i];
+
+        tableElement.append(`<tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.idNumber}</td>
+            <td>${employee.titlePosition}</td>
+            <td>${employee.salary}</td>
+        </tr>`);
+    }
+}
